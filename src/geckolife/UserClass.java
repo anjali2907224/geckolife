623714d9ddca574397a47f8357ddb305085f44ba
Amit Bhardwaj <amitbhardwaj6159@gmail.com>
2019-07-28 14:37:04 +0530

    modified

--- src/geckolife/UserClass.java
+++ src/geckolife/UserClass.java
@@ -4,6 +4,7 @@ public class UserClass {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GecoLife");
+		System.out.println("Welcome to GecoLife");
 		// TODO Auto-generated method stub
 
 	}
