1987eff2c6d62acb98e71606b9e0a920ecd44c0b
Amit Bhardwaj <amitbhardwaj6159@gmail.com>
2019-07-28 14:29:58 +0530

    added new files

---
+++ src/geckolife/UserClass.java
@@ -0,0 +1,11 @@
+package geckolife;
+
+public class UserClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to GecoLife");
+		// TODO Auto-generated method stub
+
+	}
+
+}
